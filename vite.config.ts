dbf757c651139793bc2618c4d2d756846e9dd024
Viral Vakil <phvakil@Virals-Air.lan>
2025-05-30 14:52:03 -0500

    Resolve .gitignore conflict and update deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sweep-avoid-dust/", // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
